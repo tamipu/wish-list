776fcdf4810ab7257807c118062df92e658f2d0f
Phuong TA <104944175+tamipu@users.noreply.github.com>
2024-12-02 10:53:16 +0100

    add button add wish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,30 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/module/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+newWish: string = '';
+addWish() {
+  if (this.newWish.trim()) {
+    this.items.push({ name: this.newWish, isCompleted: false });
+    this.newWish = '';
+  }
+}
   items: WishItem[] = [
     new WishItem('New car'),
     new WishItem('Trip to Hawaii', true),
     new WishItem('Mansion in the Hamptons')
   ];
   title = 'wishlist';
+  toggleItem(item: { name: string, isCompleted: boolean }) {
+    item.isCompleted = !item.isCompleted;
+  }
 }

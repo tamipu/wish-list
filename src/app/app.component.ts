1bed29add6ec6dfd4392b57e358a0fdbb4ecc166
Phuong TA <104944175+tamipu@users.noreply.github.com>
2024-12-02 11:46:28 +0100

    add filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,18 +13,28 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
 newWish: string = '';
+items: WishItem[] = [
+  new WishItem('New car'),
+  new WishItem('Trip to Hawaii', true),
+  new WishItem('Mansion in the Hamptons')
+];
+title = 'wishlist';
+filterCriteria: string = 'all';
 addWish() {
   if (this.newWish.trim()) {
     this.items.push({ name: this.newWish, isCompleted: false });
     this.newWish = '';
   }
 }
-  items: WishItem[] = [
-    new WishItem('New car'),
-    new WishItem('Trip to Hawaii', true),
-    new WishItem('Mansion in the Hamptons')
-  ];
-  title = 'wishlist';
+getFilteredItems() {
+  if (this.filterCriteria === 'completed') {
+    return this.items.filter(item => item.isCompleted);
+  } else if (this.filterCriteria === 'notCompleted') {
+    return this.items.filter(item => !item.isCompleted);
+  } else {
+    return this.items;
+  }
+}
   toggleItem(item: { name: string, isCompleted: boolean }) {
     item.isCompleted = !item.isCompleted;
   }
